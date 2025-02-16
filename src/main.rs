ed615cbddf067509d4e2ef46bea43602cbca53f0
Hung <skellybg245@gmail.com>
2025-02-16 22:52:43 +1100

    feat: add new subscription via route

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::net::TcpListener;
 
-use sqlx::{Connection, PgConnection};
+use sqlx::{Connection, PgPool};
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
 
-    let connection = PgConnection::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
 
@@ -15,5 +15,5 @@ async fn main() -> Result<(), std::io::Error> {
 
     let listener = TcpListener::bind(address)?;
 
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
